6cbf1f899e39e4d37ddf6051121b500126866e70
Alejandro Andrade <meuxdev@gmail.com>
2022-07-24 21:06:37 -0500

    Implementing the Polly -> this way we implement a quicklier response in case of an error o delay.

--- Play.Catalog.Service/Controllers/ItemsController.cs
+++ Play.Catalog.Service/Controllers/ItemsController.cs
@@ -15,6 +15,7 @@ namespace Play.Catalog.Controllers
     public class ItemsController : ControllerBase
     {
         private readonly IRepository<Item> itemsRepository;
+        private static int requestCounter = 0;
 
         public ItemsController(IRepository<Item> _itemsRepository)
         {
@@ -23,11 +24,28 @@ namespace Play.Catalog.Controllers
 
         // GET -> /api/items
         [HttpGet]
-        public async Task<IEnumerable<ItemDto>> GetAsync()
+        public async Task<ActionResult<IEnumerable<ItemDto>>> GetAsync()
         {
+            requestCounter++;
+            Console.WriteLine($"Request {requestCounter}: Starting...");
+
+            if (requestCounter <= 2)
+            {
+                Console.WriteLine($"Request {requestCounter}: Delaying...");
+                await Task.Delay(TimeSpan.FromSeconds(10));
+            }
+
+            if (requestCounter <= 4)
+            {
+                Console.WriteLine($"Request {requestCounter}: 500 (Internal Server error)...");
+                return StatusCode(500);
+            }
+
+
             var items = (await itemsRepository.GetAllAsync())
                         .Select(item => item.AsDto());
-            return items;
+            Console.WriteLine($"Request {requestCounter}: 200 (OK)");
+            return Ok(items);
         }
 
 
