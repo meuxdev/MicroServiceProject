aece5dea239bbce6c2ea4502b8fbed99247338b2
Alejandro Andrade <meuxdev@gmail.com>
2022-07-24 21:55:39 -0500

    Adding the circuit breaker, and the configuration to the startup file.

--- Play.Inventory.Service/Startup.cs
+++ Play.Inventory.Service/Startup.cs
@@ -52,6 +52,20 @@ namespace Play.Inventory.Service
                 //! --------------------
                 //! Remove in production
             ))
+            .AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().CircuitBreakerAsync(
+                handledEventsAllowedBeforeBreaking: 3, // events allowed before starting the circuit breaker
+                durationOfBreak: TimeSpan.FromSeconds(15), // Time of break before accepting more requests 
+                onBreak: (outcome, timespan) =>
+                {
+                    var serviceProvider = services.BuildServiceProvider();
+                    serviceProvider.GetService<ILogger<CatalogClient>>()?.LogWarning($"Opening the circuit for {timespan.TotalSeconds} seconds...");
+                },
+                onReset: () =>
+                {
+                    var serviceProvider = services.BuildServiceProvider();
+                    serviceProvider.GetService<ILogger<CatalogClient>>()?.LogWarning("Closing the circuit...");
+                }
+            ))
             .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1 /* Wait one second before giving up */ )); // Policy to handle external handler errors
 
             services.AddControllers();
